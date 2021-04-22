792164707798da49bdb6a32335e89807a393e84b
coderShreyash <69112238+coderShreyash@users.noreply.github.com>
2021-04-22 19:44:06 +0530

    @Superb 🎉

---
+++ script.js
@@ -0,0 +1,10 @@
+var database=firebase.database();
+function extraf(){
+    window.open("extra.html")
+}
+function blogf(){
+    document.getElementById("i1").setAttribute('src',"blog.html");
+}
+function community(){
+    document.getElementById("i1").setAttribute('src',"chaco.html");
+  }
